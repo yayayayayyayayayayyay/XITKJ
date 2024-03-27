4f53345707e536b4806c7a4a92f8d79d0b278e9e
kepoo <dhikagamergz@gmail.com>
2024-03-27 22:05:29 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+// Toggle class active untuk hamburger menu
+const navbarNav = document.querySelector('.navbar-nav');
+// ketika hamburger menu di klik
+document.querySelector('#hamburger-menu').onclick = () => {
+  navbarNav.classList.toggle('active');
+};
+
+const hm = document.querySelector('#hamburger-menu');
+
+document.addEventListener('click', function (e) {
+  if (!hm.contains(e.target) && !navbarNav.contains(e.target)) {
+      navbarNav.classList.remove('active');
+  }
+});
